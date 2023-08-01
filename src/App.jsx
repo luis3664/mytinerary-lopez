d8d6f5aaafa898b681cb1815c9e3103990791823
Luis Lopez <luis.lopez3664mail.com>
2023-07-31 22:07:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from '/react.svg'
+import viteLogo from '/vite.svg'
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <header>
+        
+      </header>
+      <main>
+        <h1>Vite + React</h1>
+      </main>
+      <footer>
+      </footer>
+    </>
+  )
+}
+
+export default App
